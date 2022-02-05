6a359037f1eeb7e565d98ff616273b9f807323b1
essumanshetty <sumanshettysumanshetty65@gmail.com>
2022-02-05 18:46:40 +0530

    Show and Highlight Search result

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 let main = document.querySelector("main");
 let arr = [];
 let search = document.getElementById("search");
+let resultContainer = document.querySelector(".resultContainer");
+let noMatch = document.querySelector("#noMatch");
 
 search.addEventListener("focus", function (e) {
-  document.querySelector(".resultContainer").style.display = "block";
+  resultContainer.style.display = "block";
 });
 
-let results = document.querySelectorAll(".result");
-for (let i = 0; i < results.length; i++) {
-  results[i].addEventListener("click", function (event) {
-    console.log("clicked...", event.target.id);
-    document.querySelector(".resultContainer").style.display = "none";
+search.addEventListener("focusout", function (e) {
+  // resultContainer.style.display = "none";
+  // noMatch.style.display = "none";
+});
+
+search.addEventListener("input", function (event) {
+  resultContainer.innerHTML = ""; //Empty div on every inp[ut]
+  // console.log(arr);
+
+  arr.forEach(({ title, body }) => {
+    // val = JSON.stringify(val);
+    // console.log(title);
+    if (
+      title.includes(event.target.value) &&
+      event.target.value != " " &&
+      event.target.value != ""
+    ) {
+      // console.log(val);
+      let resultDiv = document.createElement("div");
+      resultDiv.classList.add("result");
+      // resultDiv.id = title;
+      // resultDiv.textContent = JSON.parse(val).title;
+      resultDiv.textContent = title;
+      resultContainer.append(resultDiv);
+    } else {
+      noMatch.style.display = "block";
+    }
   });
+  temp();
+});
+
+function temp() {
+  let results = document.querySelectorAll(".result");
+
+  for (let i = 0; i < results.length; i++) {
+    results[i].addEventListener("click", function (event) {
+      // console.log("clicked...", event.target.innerHTML);
+      const current = document.getElementById(event.target.innerHTML);
+      current.scrollIntoView();
+      current.closest(".card").classList.add("blue");
+      resultContainer.style.display = "none";
+    });
+  }
 }
 
 window.onload = function () {
@@ -26,6 +65,7 @@ window.onload = function () {
         let cardHeader = document.createElement("div");
         cardHeader.classList.add("cardHeader");
         cardHeader.textContent = item.title;
+        cardHeader.id = item.title;
 
         let cardBody = document.createElement("div");
         cardBody.classList.add("cardBody");
